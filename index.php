ec766e36e05a4e5e4057bc6c71e8a45cc0415304
Anuskota <anamangada.dev@gmail.com>
2024-01-23 15:26:46 +0100

    finish Baby

--- index.php
+++ index.php
@@ -97,9 +97,154 @@ function misNumerosFavoritos() {
 print_r(misNumerosFavoritos());
 
 //Crear una función que añade un número al array
+function añadirNumeroArray(){
+    $numbers = misNumerosFavoritos();
+    array_push($numbers, 5);
+    return $numbers;
+}
+print_r(añadirNumeroArray());
+
+//Crear una función que elimine los números pares de ese array.
+
+
+function eliminarPares($numbers){
+    $numbers = misNumerosFavoritos();
+    foreach($numbers as $my_item => $value){
+        if($value % 2 === 0){
+        unset($numbers[$my_item]);
+        
+    }
+}
+    $numbers = array_values($numbers);
+    return $numbers;
+}
+
+print_r(eliminarPares($numbers));
+
+//funcion que devuelve el mayor numero de un array
+
+function numeroMayor(){
+    $numbers2 =[3,44,75,96,32,4,75,10];
+    echo max($numbers2);
+    
+}
+//Numero menor
+function numeroMenor()
+{
+    $numbers2 = [3, 44, 75, 96, 32, 4, 75, 10];
+    echo min($numbers2) . "\n";
+}
+
+numeroMenor();
+
+//CONVIERTE EN MAYUSCULAS UN TEXTO
+function mayusculas($nombre){
+    $aMayusculas = strtoupper($nombre);
+    return $aMayusculas;
+    
+}
+print_r(mayusculas("ana \n"));
+
+function minuscula($nombre)
+{
+    $aMinusculas = strtolower($nombre);
+    return $aMinusculas;
+}
+print_r(minuscula("ANA "));
+
+//Crear una función que reciba un array de nombres y que convierta la primera letra de cada nombre en mayúscula.
+function primerMayuscula($array){
+    $resultado =array();
+
+    foreach($array as $palabra){
+        $palabraCapitalizada = ucwords($palabra);
+        $resultado[] = $palabraCapitalizada;
+        
+    }
+    
+    return $resultado;
+
+    
+}
+$comidaFavorita = ["migas", "sopa castellana", "fabada asturiana", "queso cabrales"];
+print_r(primerMayuscula($comidaFavorita));
+
+
+class myCar {
+   public $marcas = ["toyota", "Skoda", "renault"];
+    
+}
+
+
+//CREAR UNA CLASE COCHE CON SUS ATRIBUTOS Y UN ATRIBUTO PUEDE SER TIPO ARRAY
+
+class Coche {
+    
+    public $marca= "renault";
+    public $numeroDePuertas=7;
+    
+    public $atributos = [
+        "marchas" => 6,
+        "ruedas" =>"michelin",
+        "asientos" => "cuero"
+            
+        
+    ];
+
+    //Constructor
+
+    public function __construct($marca,$numeroDePuertas){
+        $this -> marca = $marca; 
+        $this -> numeroDePuertas = $numeroDePuertas; 
+    }
+    
+    //Método para obtener Marca
+
+    public function getMarca(){
+    return $this -> marca ;}
+
+    //Funcion para obtener atributos
+
+
+    public function getAtributos()
+    {
+        return $this->atributos;
+    } 
+
+    //Método para saber las puertas
+    public function getPuertas(){
+        return $this -> numeroDePuertas;
+    }
+    
+    //Método para estabñecer el nombre
+
+    public function establecerNuevaMarca($nuevaMarca){
+        $this -> marca = $nuevaMarca;
+    }
+}
+
+//Nuevo coche
+
+$micoche = new Coche("Toyota", 7);
+echo "Marca de coche:" . $micoche -> getMarca() . "\n";
+echo "El número de puertas es:" . $micoche -> getPuertas();
+print_r(($micoche -> getAtributos()));
 
 
 
+
+
+
+
+
+
+// Multiplicar un numero por 8 si es par o por 9 si es impar, con if refactorizado
+
+function simpleMultiplication($number)
+{
+
+    return ($number % 2 === 0) ? $number * 8 : $number * 9;
+}
 
 
 
